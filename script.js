c84b885ce33daf7f2eff2a2e8fad0c44e1492140
Vlad Melnyk <vlad22523@gmail.com>
2024-02-16 00:46:11 +0200

    goit-markup-hw-06

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(".open-menu-btn").click(function (event) {
-    $(".header-burger").toggleClass("is-open");
+    $(".header-burger").toggleClass("active");
   });
 });
 
 $(document).ready(function () {
   $(".burger-close-btn").click(function (event) {
-    $(".header-burger").toggleClass("is-open");
+    $(".header-burger").toggleClass("active");
   });
 });

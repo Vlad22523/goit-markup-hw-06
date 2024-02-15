cfa290ea30195fe8318c0f779c1660b798dce049
Vlad Melnyk <vlad22523@gmail.com>
2024-02-16 00:21:28 +0200

    goit-markup-hw-06

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".open-menu-btn").click(function (event) {
+    $(".header-burger").toggleClass("is-open");
+  });
+});
+
+$(document).ready(function () {
+  $(".burger-close-btn").click(function (event) {
+    $(".header-burger").toggleClass("is-open");
+  });
+});
